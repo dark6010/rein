f3c844cd79442b0b682e1725cc5645f9d4a5f1af
Grover Plaza Quiroga <grover.plaza@gmail.com>
2017-06-22 15:22:38 -0400

    dssdd

--- routes/index.js
+++ routes/index.js
@@ -28,18 +28,51 @@ router.post('/plantilla', function(req, res) {
   var acum=0;
   var hr= new Array(req.body.no);
   var mr= new Array(req.body.no);
+  console.log(hr.length)
   for (i = 0; i < req.body.hfp.length ; i++) {
-    while(((req.body.hip[i]*60+req.body.mip[i])-(req.body.hi*60+req.body.mi*1))> horarios[pos] && flag){
-      hr[pos]=req.body.hi*1+Math.floor(horarios[pos]/60)
-      if(horarios[pos]%60+req.body.mi*1 > 59){
-        hr[pos]++;
-        mr[pos]=((horarios[pos]+req.body.mi*1)%60)
-      }else{
-        mr[pos]=(horarios[pos]%60+req.body.mi*1)
+    if(i==0){
+      while(flag && ((req.body.hip[i]*60+req.body.mip[i])-(req.body.hi*60+req.body.mi*1))>= horarios[pos] ){
+        hr[pos]=req.body.hi*1+Math.floor(horarios[pos]/60)
+        if(horarios[pos]%60+req.body.mi*1 > 59){
+          hr[pos]++;
+          mr[pos]=((horarios[pos]+req.body.mi*1)%60)
+        }else{
+          mr[pos]=(horarios[pos]%60+req.body.mi*1)
+        }
+        if(++pos == req.body.no*1)
+          flag=false
       }
-      
+      acum=(req.body.hip[i]*60+req.body.mip[i])-(req.body.hi*60+req.body.mi*1);
+    }
+    if(i<= req.body.hfp.length-1 && i>0){
+      while(flag && ((req.body.hip[i]*60+req.body.mip[i])-(req.body.hfp[i-1]*60+req.body.mfp[i-1]))>= horarios[pos]-acum ){
+        hr[pos]=req.body.hfp[i-1]+Math.floor((horarios[pos]-acum)/60)
+        if(horarios[pos]%60+req.body.mfp[i-1] > 59){
+          hr[pos]++;
+          mr[pos]=(horarios[pos]%60+req.body.mfp[i-1])%60
+        }else{
+          mr[pos]=(horarios[pos]%60+req.body.mfp[i-1])
+        }
+        if(++pos == req.body.no*1)
+          flag=false
+      }
+    }
+    if(i+1==horarios.length){
+      while(flag && ((req.body.hf*60+req.body.mf*1)-(req.body.hfp[i]*60+req.body.mfp[i]))>= horarios[pos]-acum ){
+        hr[pos]=req.body.hfp[i]+Math.floor((horarios[pos]-acum)/60)
+        if((horarios[pos]-acum)%60+req.body.mfp[i] > 59){
+          hr[pos]++;
+          mr[pos]=((horarios[pos]-acum+req.body.mfp[i])%60)
+        }else{
+          mr[pos]=(horarios[pos]-acum)%60+req.body.mfp[i]
+        }
+        if(++pos == req.body.no*1)
+          flag=false
+      }
     }
   }
+  console.log(hr)
+  console.log(mr)
 });
 
 
